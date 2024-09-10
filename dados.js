6b9a63c0020e20edf0dfbbc76881fd3b6ab17ca2
honeybees2 <133404750+honeybees2@users.noreply.github.com>
2024-09-09 21:36:59 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,37 @@
+let dados = [
+    {
+        titulo : "Tinker Bell",
+        descricao : "Sininho, ou Tinker Bell, é uma das fadas mais conhecidas e adoradas do mundo. Criada por J.M. Barrie para a peça Peter Pan, ela é uma pequena fada com um temperamento forte e um coração de ouro. Sininho é conhecida por sua rivalidade com Wendy, sua paixão por Peter Pan e por sua habilidade em consertar objetos. Sua imagem como uma fada travessa e cheia de personalidade a tornou um ícone da cultura pop, aparecendo em diversas adaptações de Peter Pan e em diversas outras mídias.",
+        link : "https://pt.wikipedia.org/wiki/Sininho"
+    },
+    {
+        titulo: "Fada Madrinha",
+        descricao: "A fada madrinha é uma figura icônica nos contos de fadas, representando a esperança, a transformação e a magia. Geralmente descrita como uma figura benevolente e poderosa, ela surge nos momentos mais difíceis para conceder desejos e ajudar seus afilhados a alcançar seus objetivos. Com sua varinha mágica e um toque de poeira de estrelas, a fada madrinha é capaz de realizar feitos extraordinários, como transformar abóboras em carruagens e ratos em cocheiros. Sua presença nos contos de fadas simboliza a crença na bondade e na possibilidade de sonhos se tornarem realidade.",
+        link: "https://pt.wikipedia.org/wiki/Fada_Madrinha"
+    },
+    {
+        titulo: "Titânia",
+        descricao: "Titânia é a rainha das fadas na peça de William Shakespeare 'Sonho de uma Noite de Verão'. Conhecida por sua beleza e temperamento forte, ela é frequentemente retratada como uma figura associada à natureza e à magia. Titânia é envolvida em uma disputa com Oberon, o rei das fadas, que leva a uma série de eventos mágicos e hilários.",
+        link: "https://pt.wikipedia.org/wiki/Sonho_de_uma_Noite_de_Verão"
+    },
+    {
+        titulo: "Oberon",
+        descricao: "Oberon é o rei das fadas na peça de William Shakespeare 'Sonho de uma Noite de Verão'. Ele é um personagem poderoso e manipulador, envolvido em uma disputa com sua esposa, Titânia. Oberon utiliza a magia para alcançar seus objetivos, causando confusão e caos entre os mortais e as fadas.",
+        link: "https://pt.wikipedia.org/wiki.Sonho_de_uma_Noite_de_Verão"
+    },
+    {
+        titulo: "Winx Club",
+        descricao: "As Winx Club são um grupo de fadas adolescentes que frequentam a Escola de Magia Alfea. Elas usam seus poderes mágicos para proteger o mundo mágico de vilões e para ajudar seus amigos. As Winx Club se tornaram um fenômeno global, com séries de animação, filmes, brinquedos e jogos.",
+        link: "https://pt.wikipedia.org/wiki/Winx_Club"
+    },
+    {
+        titulo: "Morgana Le Fay",
+        descricao: "Morgana Le Fay, também conhecida como Morgana, é uma figura complexa da mitologia arturiana, muitas vezes retratada como uma poderosa feiticeira e meia-irmã do Rei Arthur. Ela é frequentemente associada à magia, à natureza e à sabedoria.",
+        link: "https://pt.wikipedia.org/wiki/Fada_Morgana"
+    },
+    {
+        titulo: "Yvaine",
+        descricao: "Yvaine, da animação 'Stardust', é uma estrela que cai na Terra e se transforma em uma bela jovem. Ela possui poderes mágicos e é perseguida por vários homens que desejam seus poderes. Yvaine é um símbolo de pureza e beleza.",
+        link: "https://stardust.fandom.com/wiki/Yvaine" // A página da Wikipédia sobre Yvaine pode estar principalmente em inglês
+    }
+]
